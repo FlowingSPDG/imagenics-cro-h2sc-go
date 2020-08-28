0958a2b57018c8442fc9b7bab8468d091c08f8aa
Shugo Kawamura <kawamura@giken.tv>
2020-08-28 10:24:04 +0900

    Added ResetID() command.

--- id.go
+++ id.go
@@ -24,3 +24,24 @@ func (h *H2SC) Identify(id string) error {
 	h.id = id
 	return nil
 }
+
+// ResetID Reset H2SC ID.
+func (h *H2SC) ResetID() error {
+	packet := newPacket()
+	packet.command = COMMAND_IDENTIFY
+	packet.idnum = "00"
+	packet.param = []byte("00000") // e.g. +0001
+
+	ps := packet.ToSlice()
+	if err := checkPacketLength(ps); err != nil {
+		return err
+	}
+	h.conn.SetDeadline(time.Now().Add(timeout))
+	_, err := h.conn.Write(ps)
+	if err != nil {
+		return err
+	}
+
+	h.id = "00"
+	return nil
+}
